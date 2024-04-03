075d65c959406e85b9eb558dc5134a917c461a5c
mahesh <mahesh@gmail.com>
2024-04-03 09:07:32 +0530

    changes made

---
+++ controllers/fetch-controller/get-search-images.js
@@ -0,0 +1,39 @@
+import ImageModel from "../../schema/img-schema.js";
+
+const getSearchedImgs = async (req, res, next) => {
+    try{
+
+        const {pageNumber, pageSize, searchedWords, aspectRatio} = req.body
+
+        const offset = (pageNumber - 1 ) * pageSize
+        const limit = pageSize
+        let imageData
+
+        if(searchedWords){
+
+            imageData = await ImageModel.find({
+                'keyWords' : {$in : searchedWords},
+                'aspectRatio' : aspectRatio
+            }).skip(offset).limit(limit)
+        }
+        else{
+            imageData = await ImageModel.find({
+                aspectRatio : aspectRatio
+            }).skip(offset).limit(limit)
+        }
+
+        res.status(200).json({success : true, data : imageData})
+
+    }
+    catch(error)
+    {
+        const err = new Error()
+        err.name = error.name || null
+        err.message = error.message || null
+        err.status = error.status || null
+        
+        next(err)
+    }
+}
+
+export default getSearchedImgs
--- router/index-router.js
+++ router/index-router.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import getAllImages from '../controllers/fetch-controller/get-all-images.js'
+import getSearchedImgs from '../controllers/fetch-controller/get-search-images.js'
 
 import addNewImg from '../controllers/add-controller/add-new-img.js'
 import addExistingImg from '../controllers/add-controller/add-existing-img.js'
@@ -12,6 +13,7 @@ const indexRouter= express()
 indexRouter.route('/health').get((req, res)=>{res.send("Api is healthy")})
 
 indexRouter.route('/get-all-imgs').post(getAllImages)
+indexRouter.route('/get-searched-imgs').post(getSearchedImgs)
 
 indexRouter.route('/add-new-img').post(upload.single("imageFile"), addNewImg)
 indexRouter.route('/add-existing-img').post(addExistingImg)
